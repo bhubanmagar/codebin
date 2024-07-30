f6e0092abd1345aabcf6135031d3c4c108d187c3
smash3r <bhubanmagar79@gmail.com>
2024-07-30 15:03:57 +0545

    user component

---
+++ src/app/Components/user-profile/user-profile.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css',
+})
+export class UserProfileComponent {
+  name = 'bhuban';
+  inputValue = 'test';
+
+  // onChange(e: Event) {
+  //   const value = (e.target as HTMLInputElement).value;
+  //   this.inputValue = value;
+  // }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfileComponent } from './Components/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'codebin';
